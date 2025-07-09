f68ebb3b63e40e5ddce2240b786ac0e00cd19a7e
Santhosh2141 <snthshsrinivas@gmail.com>
2025-07-09 19:58:23 +0530

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Santhosh Srinivas on 09/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
